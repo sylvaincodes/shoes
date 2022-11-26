6d8adaf6a2d3beeebada8e4980ac1dde37ed7755
sylvaincodes <adandesylvain2010@gmail.com>
2022-11-26 05:11:18 +0100

    intégration page cart

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { store } from "./redux/store";
-import reportWebVitals from "./reportWebVitals";
-import { Provider } from "react-redux";
 import productsData from "./data/short-product.json";
-import "./assets/fonts/font.css";
 import "./assets/css/style.css";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import "./assets/fonts/font.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 store.dispatch({
   type: "FETCH_PRODUCTS",
   payload: { products: productsData },
 });
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
 root.render(
-  <React.StrictMode>
-       <Provider store={store}>
+  <Provider store={store}>
       <App />
        </Provider>
-    </React.StrictMode>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/others/Cart.jsx
+++ src/pages/others/Cart.jsx
@@ -6,6 +6,7 @@ import BreadcrumbsItem from "../../components/breadcrumbs/BreadcrumbsItem";
 import { AiFillCloseCircle } from 'react-icons/ai'
 
 import { useSelector,useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Cart = ({strings}) => {
 
@@ -13,6 +14,16 @@ const Cart = ({strings}) => {
     const total = useSelector((state) => state.cartData.total);
     const dispatch = useDispatch();
 
+    const handleDeleteProduct = (product) =>{
+      dispatch({
+        type: "DELETE_FROM_CART",
+        payload: {
+          product : product
+        },
+      });
+    }
+
+    
   return (
     <Fragment>
       <Helmet>
@@ -34,18 +45,19 @@ const Cart = ({strings}) => {
           <div className="container">
             <div className="wrapper">
               <table>
-                <thead>
-                  <tr>
+             
+
+  
+                <tbody>
+          
+                  {/* <tr>
                     <th>Image</th>
                     <th>Name</th>
                     <th>Price</th>
                     <th>Qty</th>
                     <th>Sub total</th>
                     <th>Action</th>
-                  </tr>
-                </thead>
-
-                <tbody>
+                  </tr> */}
 
                     {
                         products ? products.map( (product) =>{
@@ -54,14 +66,20 @@ const Cart = ({strings}) => {
                                 <td>
                                     <div className="flex-column">
                                         <h6>{product.name}</h6>
-                                        <span >Color : {product.selectedProductColor}</span>
-                                        <span >Size : {product.selectedProductSize}</span>
+                                        <span >{strings['color']}  : {product.selectedProductColor}</span>
+                                        <span >{strings['size']}  : {product.selectedProductSize}</span>
                                     </div>
                                 </td>   
                                 <td className="price">{(product.total/product.qtySelected).toFixed(3)}</td>
                                 <td className="qty"> {product.qtySelected}</td>
                                 <td className="total"> {product.total.toFixed(3)}</td>
-                                <td><AiFillCloseCircle  className="icon shopping-cart-delete" /></td>
+                                <td>
+                                  <button className="icon" onClick={()=>handleDeleteProduct(product)}>
+                                  <AiFillCloseCircle  className="shopping-cart-delete" />
+                                  {strings['remove']} 
+                                  </button>
+                                </td>
+                               
                             </tr>
                         })
                         : ""
@@ -70,6 +88,42 @@ const Cart = ({strings}) => {
 
                 </tbody>
               </table>
+
+              <div className="cart-actions">
+                <form action="" method="post">
+                  <div className="row">
+                  <input className="" type="text" placeholder="coupon code" />
+                  <button className="btn">{strings['apply']} </button>
+                  </div>
+
+                  <div className="row">
+                    {/* <button className="btn">Update cart</button> */}
+                    <button className="btn">
+                       <Link to="/shop">
+                       {strings['continue_shopping']} 
+                       </Link>
+                       </button>
+                  </div>
+
+                </form>
+              </div>
+
+              <div className="cart-summary">
+                <form action="" method="post">
+
+                    <h6>{strings['cart_totals']} </h6>
+                    <ul>
+                      <li><strong>{strings['subtotal']} </strong>  <span>$ {total.toFixed(3) }</span></li>
+                      <li><strong>{strings['shippings']} </strong>  <span>+ $10</span></li>
+                      <li><strong>{strings['tax']} </strong>  <span>+ $0</span></li>
+                      <li><strong>{strings['total']} </strong>  <span>$ { (total  +10 ).toFixed(3) } </span></li>
+                    </ul>
+                    <div className="row">
+                    <button className="btn">{strings['checkout']} </button>
+                    
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
